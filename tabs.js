9dc973714b5458e1e15d1ebf65f2313e032feab8
Olesia <okissaw@gmail.com>
2021-12-26 19:42:08 +0200

    Add tabs script

---
+++ tabs.js
@@ -0,0 +1,55 @@
+const tabList = document.querySelector("[role='tablist']");
+const tabs = document.querySelectorAll("[role='tab']");
+
+
+tabList.addEventListener("keydown", changeTabFocus);
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', changeTabPanel);
+});
+
+let tabFocus = 0;
+function changeTabFocus(e) {
+  const keydownLeft = 37;
+  const keydownRight = 39;
+
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabs[tabFocus].setAttribute("tabindex", -1);
+  }
+
+  if (e.keyCode === keydownRight) {
+    tabFocus++;
+    if (tabFocus >= tabs.length) {
+      tabFocus = 0;
+    }
+    console.log(tabFocus);
+  }
+
+  if (e.keyCode === keydownLeft) {
+    tabFocus--;
+    if (tabFocus < 0) {
+      tabFocus = tabs.length - 1;
+    }
+    console.log(tabFocus);
+  }
+
+  tabs[tabFocus].setAttribute("tabindex", 0);
+  tabs[tabFocus].focus();
+}
+
+function changeTabPanel(e) {
+  const targetTab = e.target;
+  console.log(targetTab);
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  console.log(targetPanel);
+
+  const tabContainer = targetTab.parentNode;
+  const mainContainer = tabContainer.parentNode;
+
+  mainContainer
+            .querySelectorAll("[role='tabpanel']")
+            .forEach((panel) => panel.setAttribute("hidden", true));
+
+  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+
+}
